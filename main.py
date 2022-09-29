25eaaf021629c51d5082fbcf68d82b037f28325e
andrey <andreywill9@gmail.com>
2022-09-29 19:52:21 -0300

    Criando coluna de tags

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from typing import List
+
 import pandas
 
 # LIMPEZA
+
+termos_candidatos = {
+    'Lula': ['lula', 'luiz inacio lula da silva'],
+    'Bolsonaro': ['bolsonaro', 'jair messias bolsonaro']
+}
+
+def obter_tags(texto_tweet: str) -> List[str]:
+    tags_tweet = []
+    for candidato, termos in termos_candidatos.items():
+        if any(map((lambda termo: termo in texto_tweet), termos)):
+            tags_tweet.append(candidato)
+    return tags_tweet
+
 
 dataframe = pandas.read_excel('bases/base-menor.xlsx')
 
@@ -18,5 +33,7 @@ dataframe['mes'] = dataframe['data_tweet'].map(lambda data: data.month)
 dataframe['semana_ano'] = dataframe['data_tweet'].map(lambda data: int(data.strftime("%V")))
 
 dataframe['semana_analise'] = dataframe['semana_ano'] - 22
+
+dataframe['tags'] = dataframe['texto'].map(obter_tags)
 
 dataframe.to_excel('bases/base-menor-tratada.xlsx')
