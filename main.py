8604db75082b97e5d295f016801aa56482d071b3
andrey <andreywill9@gmail.com>
2022-10-06 18:15:50 -0300

    Criação de coluna especifica para a citação de cada candidato

--- main.py
+++ main.py
@@ -5,29 +5,20 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-
-def obter_tags(texto_tweet: str) -> List[str]:
-    tags_tweet = []
-    for candidato, termos in termos_candidatos.items():
-        if any(map((lambda termo: termo in texto_tweet), termos)):
-            tags_tweet.append(candidato)
-    return tags_tweet
-
 # LIMPEZA
 
 termos_candidatos = {
-    'Lula': ['lula', 'luiz inacio lula da silva'],
-    'Bolsonaro': ['bolsonaro', 'jair messias bolsonaro'],
-    'Ciro': ['ciro', 'ciro gomes'],
-    'Simone Tebet': ['tebet', 'simone tebet'],
-    'Eymael': ['eymael', 'eymael o democrata cristao'],
-    'Sofia Manzano': ['sofia manzano', 'manzano pcb'],
-    'Soraya Thronicke': ['soraya thronicke', 'soraya união brasil'],
-    'Felipe D’Avila': ['felipe davila', 'felipe davila novo'],
-    'Vera Lúcia': ['vera lucia','vera lucia pstu'],
-    'Leo Péricles': ['leo pericles', 'leonardo pericles']
+    'lula': ['lula', 'luiz inacio lula da silva'],
+    'bolsonaro': ['bolsonaro', 'jair messias bolsonaro'],
+    'ciro': ['ciro', 'ciro gomes'],
+    'simone_tebet': ['tebet', 'simone tebet'],
+    'eymael': ['eymael', 'eymael o democrata cristao'],
+    'sofia_manzano': ['sofia manzano', 'manzano pcb'],
+    'soraya_thronicke': ['soraya thronicke', 'soraya união brasil'],
+    'felipe_d_avila': ['felipe davila', 'felipe davila novo'],
+    'vera_lucia': ['vera lucia','vera lucia pstu'],
+    'leo_pericles': ['leo pericles', 'leonardo pericles']
 }
-
 
 dataframe = pandas.read_excel('bases/base-menor.xlsx')
 
@@ -40,30 +31,34 @@ dataframe['texto'] = dataframe.texto.str\
 dataframe['data_tweet'] = pandas.to_datetime(dataframe['data_tweet'])\
     .dt.tz_localize(None)
 
-dataframe['mes'] = dataframe['data_tweet'].map(lambda data: data.month)
+print(dataframe.head())
+
+dataframe['mes'] = dataframe['data_tweet'].dt.month
 
 dataframe['semana_ano'] = dataframe['data_tweet'].map(lambda data: int(data.strftime("%V")))
 
 dataframe['semana_analise'] = dataframe['semana_ano'] - 22
 
-dataframe['tags'] = dataframe['texto'].map(obter_tags)
+for chave, valor in termos_candidatos.items():
+    termos_regex = '|'.join(valor)
+    dataframe[chave] = dataframe['texto'].str.contains(pat=fr'(?:{termos_regex.lower()})', case=False).map({True: 1,
+                                                                                                            False: 0})
+
+dataframe.to_excel('bases/base-menor-tratada.xlsx')
 
 
-def mostrar_contagem_citacoes() -> None:
-    todas_citacoes = {}
-    for candidato in termos_candidatos.keys():
-        contagem = 0
-        if candidato not in todas_citacoes:
-            todas_citacoes[candidato] = 0
-        for tags in dataframe.tags:
-            if candidato in tags:
-                contagem += 1
-        todas_citacoes[candidato] = contagem
-    todas_citacoes = {k: v for k, v in todas_citacoes.items() if v > 0}
-
-    plt.pie(todas_citacoes.values(), labels=todas_citacoes.keys())
-    plt.show()
-
-
-mostrar_contagem_citacoes()
+# def mostrar_contagem_citacoes() -> None:
+#     todas_citacoes = {}
+#     for candidato in termos_candidatos.keys():
+#         contagem = 0
+#         if candidato not in todas_citacoes:
+#             todas_citacoes[candidato] = 0
+#         for tags in dataframe.tags:
+#             if candidato in tags:
+#                 contagem += 1
+#         todas_citacoes[candidato] = contagem
+#     todas_citacoes = {k: v for k, v in todas_citacoes.items() if v > 0}
+#
+#     plt.pie(todas_citacoes.values(), labels=todas_citacoes.keys())
+#     plt.show()
 
