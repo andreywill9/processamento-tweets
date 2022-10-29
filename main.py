048af2256665781c95950a1a96f26028a8233b25
andrey <andreywill9@gmail.com>
2022-10-28 21:38:49 -0300

    Refatoração de código

--- main.py
+++ main.py
@@ -86,81 +86,53 @@ stopwords = nltk.corpus.stopwords.words("portuguese")
 palavras_irrelevantes = [*punctuation] + stopwords
 token_pontuacao = tokenize.WordPunctTokenizer()
 
-dataframe = pandas.read_excel('bases/base-menor.xlsx')
+dataframe = pandas.read_excel('bases/base-completa.xlsx')
 
-dataframe['texto'] = dataframe.texto.str \
-    .lower() \
-    .replace(r'[^\w\s@]', '', regex=True) \
-    .replace(r'[\s]+', ' ', regex=True)
 
-frases_processadas = list()
-for opiniao in dataframe["texto"]:
-    nova_frase = list()
-    palavras_texto = token_pontuacao.tokenize(opiniao)
-    for palavra in palavras_texto:
-        nova_frase.extend(abreviacoes[palavra].split(' ') if palavra in abreviacoes.keys() else [palavra])
-    frases_processadas.append(' '.join(nova_frase))
-dataframe['texto'] = frases_processadas
-
-mencoes_candidatos = list()
-candidatos = list()
-
-tweets_tratados = list()
-for opiniao in dataframe.texto:
-    mencoes_tweet = re.findall("@[A-Za-z0-9_]+", opiniao)
-    outras_mencoes = list(filter((lambda x: x not in arroba_candidato), mencoes_tweet))
-
-    for mencao in outras_mencoes:
-        opiniao = opiniao.replace(mencao, '')
-    for arroba in arroba_candidato.keys():
-        opiniao = opiniao.replace(arroba, arroba_candidato[arroba])
-
-    nova_frase = list()
-    palavras_texto = token_espaco.tokenize(opiniao)
-    for palavra in palavras_texto:
-        if palavra not in stopwords:
-            nova_frase.append(palavra)
-    tweets_tratados.append(' '.join(nova_frase))
-
-dataframe["tratamento_1"] = tweets_tratados
-
-sem_acentos = [unidecode.unidecode(texto) for texto in dataframe["tratamento_1"]]
-dataframe["tratamento_2"] = sem_acentos
-
-frases_processadas = list()
-for opiniao in dataframe["tratamento_2"]:
-    nova_frase = list()
-    palavras_texto = token_pontuacao.tokenize(opiniao)
-    for palavra in palavras_texto:
-        if palavra not in palavras_irrelevantes:
-            nova_frase.append(palavra)
-    frases_processadas.append(' '.join(nova_frase))
-
-dataframe["tratamento_2"] = frases_processadas
-
-frases_processadas = list()
-for opiniao in dataframe["tratamento_2"]:
-    nova_frase = list()
-    doc = nlp(opiniao)
-    for palavra in doc:
-        nova_frase.append(palavra.lemma_.lower())
-    frases_processadas.append(' '.join(nova_frase))
-
-dataframe["tratamento_3"] = frases_processadas
-
-dataframe['data_tweet'] = pandas.to_datetime(dataframe['data_tweet']) \
-    .dt.tz_localize(None)
-
-dataframe['mes'] = dataframe['data_tweet'].dt.month
-
-dataframe['semana_ano'] = dataframe['data_tweet'].map(lambda data: int(data.strftime("%V")))
-
-dataframe['semana_analise'] = dataframe['semana_ano'] - 22
-
-for chave, valor in termos_candidatos.items():
-    termos_regex = '|'.join(valor)
-    dataframe[chave] = dataframe['texto'].str.contains(pat=fr'(?:{termos_regex.lower()})', case=False).map({True: 1,
-                                                                                                            False: 0})
+def tratar_base():
+    dataframe['texto'] = dataframe.texto.str \
+        .lower() \
+        .replace(r'[^\w\s@]', '', regex=True) \
+        .replace(r'[\s]+', ' ', regex=True)
+    frases_processadas = list()
+    for opiniao in dataframe["texto"]:
+        nova_frase = list()
+        mencoes_tweet = re.findall("@[A-Za-z0-9_]+", opiniao)
+        outras_mencoes = list(filter((lambda x: x not in arroba_candidato), mencoes_tweet))
+        palavras_texto = token_pontuacao.tokenize(opiniao)
+        for palavra in palavras_texto:
+            if palavra in outras_mencoes or palavra in stopwords:
+                continue
+            if palavra in arroba_candidato:
+                nova_frase.append(arroba_candidato[palavra])
+                continue
+            palavra = unidecode.unidecode(palavra)
+            if palavra in palavras_irrelevantes:
+                continue
+            nova_frase.extend(abreviacoes[palavra].split(' ') if palavra in abreviacoes.keys() else [palavra])
+        doc = nlp(' '.join(nova_frase))
+        nova_frase = list()
+        for palavra in doc:
+            nova_frase.append(palavra.lemma_.lower())
+        frases_processadas.append(' '.join(nova_frase))
+    dataframe['texto_tratado'] = frases_processadas
+    dataframe['data_tweet'] = pandas.to_datetime(dataframe['data_tweet']) \
+        .dt.tz_localize(None)
+    dataframe['mes'] = dataframe['data_tweet'].dt.month
+    dataframe['semana_ano'] = dataframe['data_tweet'].map(lambda data: int(data.strftime("%V")))
+    dataframe['semana_analise'] = dataframe['semana_ano'] - 22
+    for chave, valor in termos_candidatos.items():
+        termos_regex = '|'.join(valor)
+        dataframe[chave] = dataframe['texto_tratado'].str.contains(pat=fr'(?:{termos_regex.lower()})', case=False).map(
+            {True: 1,
+             False: 0})
+    dataframe['mais_de_um_candidato'] = dataframe['lula'] + dataframe['bolsonaro'] + dataframe['ciro'] \
+                                        + dataframe['simone_tebet'] + dataframe['eymael'] \
+                                        + dataframe['sofia_manzano'] + dataframe['soraya_thronicke'] \
+                                        + dataframe['felipe_d_avila'] + dataframe['vera_lucia'] \
+                                        + dataframe['leo_pericles']
+    dataframe['mais_de_um_candidato'] = dataframe['mais_de_um_candidato'] > 1
+    dataframe.to_excel('bases/base-tratada.xlsx')
 
 
 def mostrar_contagem_citacoes() -> None:
@@ -218,7 +190,7 @@ def mostrar_rival_natural() -> None:
 
 
 def mostrar_grafico_palavras(quantidade):
-    todas_palavras = ' '.join([texto for texto in dataframe['tratamento_3']])
+    todas_palavras = ' '.join([texto for texto in dataframe['texto_tratado']])
     token_frase = token_espaco.tokenize(todas_palavras)
     frequencia = nltk.FreqDist(token_frase)
     df_frequencia = pandas.DataFrame({"palavra": list(frequencia.keys()), "frequencia": list(frequencia.values())})
@@ -243,9 +215,21 @@ def mostrar_nuvem_palavras():
     plt.show()
 
 
-mostrar_contagem_citacoes()
-mostrar_citacoes_por_mes()
-mostrar_rival_natural()
-mostrar_grafico_palavras(10)
-mostrar_nuvem_palavras()
+def mostrar_tweets_unicos_ou_conjunto():
+    quantidade_tweets_unicos = len(dataframe.query('mais_de_um_candidato == 0'))
+    quantidade_tweets_conjunto = len(dataframe.query('mais_de_um_candidato == 1'))
+    legenda = 'unico', 'conjunto'
+    plt.pie([quantidade_tweets_unicos, quantidade_tweets_conjunto], labels=legenda,
+            autopct='%1.1f%%',
+            shadow=True)
+    plt.show()
+
+
+tratar_base()
+# mostrar_contagem_citacoes()
+# mostrar_citacoes_por_mes()
+# mostrar_rival_natural()
+# mostrar_grafico_palavras(10)
+# mostrar_nuvem_palavras()
+# mostrar_tweets_unicos_ou_conjunto()
 
