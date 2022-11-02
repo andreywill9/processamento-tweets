670f53438582fc89886e55913e60ea99869973d9
andreywill9 <70452441+andreywill9@users.noreply.github.com>
2022-11-02 20:17:20 -0300

    Implementação de nova classificação

--- main.py
+++ main.py
@@ -81,23 +81,394 @@ abreviacoes = {
     'p': 'para',
     'mt': 'muito',
     'mto': 'muito',
-    'mta': 'muita'
+    'mta': 'muita',
+    'd': 'de',
+    'tar': 'estar',
+    'tb': 'tambem',
+    'tbm': 'tambem'
+}
+
+palavras_classificadas = {
+  "amar":2,
+  "amor":2,
+  "apoiar": 2,
+  "argumento": 2,
+  "bem": 2,
+  "bom": 2,
+  "brabo": 2,
+  "certeza": 2,
+  "certo": 2,
+  "conseguir": 2,
+  "coragem": 2,
+  "cristao": 2,
+  "defender": 2,
+  "democracia": 2,
+  "deus": 2,
+  "direito": 2,
+  "eleger": 2,
+  "esperanca": 2,
+  "familia": 2,
+  "fato": 2,
+  "feliz": 2,
+  "ganhar": 2,
+  "gostar": 2,
+  "honesto": 2,
+  "importante": 2,
+  "inteligente": 2,
+  "jesus": 2,
+  "liberdade": 2,
+  "lider": 2,
+  "livre": 2,
+  "lulanoprimeiroturno": 2,
+  "lulapresidente13": 2,
+  "melhor": 2,
+  "melhorar": 2,
+  "muito": 2,
+  "otimo": 2,
+  "otimos": 2,
+  "pai": 2,
+  "paz": 2,
+  "plano": 2,
+  "prefiro": 2,
+  "proteger": 2,
+  "reforma": 2,
+  "respeitar": 2,
+  "respeito": 2,
+  "salvar": 2,
+  "sim": 2,
+  "trabalhador": 2,
+  "uniao": 2,
+  "vencer": 2,
+  "verdade": 2,
+  "verdadeiro": 2,
+  "vida": 2,
+  "vitoria": 2,
+  "1o": 1,
+  "2o": 1,
+  "abrir": 1 ,
+  "aceitar": 1 ,
+  "achar": 1 ,
+  "acontecer": 1 ,
+  "acordar": 1 ,
+  "acreditar": 1 ,
+  "ajudar": 1 ,
+  "alianca": 1 ,
+  "amigo": 1 ,
+  "apoiador": 1 ,
+  "apoio": 1 ,
+  "aprender": 1 ,
+  "aprovar": 1 ,
+  "aumentar": 1 ,
+  "auxilio": 1 ,
+  "brasil": 1 ,
+  "brasileiro": 1 ,
+  "capaz": 1 ,
+  "carater": 1 ,
+  "chance": 1 ,
+  "chegar": 1 ,
+  "claro": 1 ,
+  "comemorar": 1 ,
+  "comer": 1 ,
+  "concordar": 1 ,
+  "confirmar": 1 ,
+  "conhecer": 1 ,
+  "considerar": 1 ,
+  "continuar": 1 ,
+  "crescer": 1 ,
+  "criar": 1 ,
+  "declarar": 1 ,
+  "demais": 1 ,
+  "democrata": 1 ,
+  "democratico": 1 ,
+  "dever": 1 ,
+  "educacao": 1 ,
+  "eleicao": 1 ,
+  "eleitor": 1 ,
+  "entender": 1 ,
+  "escolher": 1 ,
+  "estrategia": 1 ,
+  "estudar": 1 ,
+  "favor": 1 ,
+  "forca": 1 ,
+  "forte": 1 ,
+  "futuro": 1 ,
+  "garantir": 1 ,
+  "governar": 1 ,
+  "governo": 1 ,
+  "grande": 1 ,
+  "homem": 1 ,
+  "ideia": 1 ,
+  "igreja": 1 ,
+  "inocente": 1 ,
+  "investigar": 1 ,
+  "irmao": 1 ,
+  "lei": 1 ,
+  "lutar": 1 ,
+  "mae": 1 ,
+  "maioria": 1 ,
+  "mandato": 1 ,
+  "merecer": 1 ,
+  "mito": 1 ,
+  "moral": 1 ,
+  "mostrar": 1 ,
+  "mudar": 1 ,
+  "mulher": 1 ,
+  "ne": 1 ,
+  "novo": 1 ,
+  "parabem": 1 ,
+  "patriota": 1 ,
+  "pensar": 1 ,
+  "populacao": 1 ,
+  "povo": 1 ,
+  "preparar": 1 ,
+  "presidencia": 1 ,
+  "presidente": 1 ,
+  "projeto": 1 ,
+  "prometer": 1 ,
+  "proposta": 1 ,
+  "provar": 1 ,
+  "querer": 1 ,
+  "razao": 1 ,
+  "realmente": 1 ,
+  "reeleger": 1 ,
+  "representar": 1 ,
+  "resolver": 1 ,
+  "rico": 1 ,
+  "saber": 1 ,
+  "saude": 1 ,
+  "sempre": 1 ,
+  "serio": 1 ,
+  "servir": 1 ,
+  "simples": 1 ,
+  "simplesmente": 1 ,
+  "social": 1 ,
+  "subir": 1 ,
+  "tanto": 1 ,
+  "tentar": 1 ,
+  "trabalhar": 1 ,
+  "trabalho": 1 ,
+  "unico": 1 ,
+  "unir": 1 ,
+  "util": 1 ,
+  "valer": 1 ,
+  "vender": 1 ,
+  "viver": 1 ,
+  "vivo": 1 ,
+  "vontade": 1 ,
+  "votar": 1 ,
+  "vote": 1 ,
+  "voto": 1 ,
+  "acusar": -1,
+  "aguentar": -1,
+  "ainda": -1,
+  "atacar": -1,
+  "ataque": -1,
+  "bater": -1,
+  "bilhoes": -1,
+  "bolha": -1,
+  "bolsonarismo": -1,
+  "bolsonarista": -1,
+  "cair": -1,
+  "cirista": -1,
+  "cobrar": -1,
+  "contrario": -1,
+  "cpi": -1,
+  "culpar": -1,
+  "deixar": -1,
+  "derrotar": -1,
+  "derrubar": -1,
+  "desculpa": -1,
+  "desesperar": -1,
+  "desistir": -1,
+  "dificil": -1,
+  "dinheiro": -1,
+  "doente": -1,
+  "drastico": -1,
+  "engracado": -1,
+  "entregar": -1,
+  "esquecer": -1,
+  "estragar": -1,
+  "falta": -1,
+  "faltar": -1,
+  "fechar": -1,
+  "feio": -1,
+  "ficar": -1,
+  "fugir": -1,
+  "fundo": -1,
+  "indeciso": -1,
+  "indicar": -1,
+  "indignar": -1,
+  "infelizmente": -1,
+  "lavar": -1,
+  "mandar": -1,
+  "menos": -1,
+  "mente": -1,
+  "militancia": -1,
+  "militante": -1,
+  "nada": -1,
+  "nenhum": -1,
+  "news": -1,
+  "ninguem": -1,
+  "nunca": -1,
+  "obrigar": -1,
+  "oposicao": -1,
+  "parar": -1,
+  "partido": -1,
+  "passado": -1,
+  "pena": -1,
+  "pequeno": -1,
+  "petista": -1,
+  "polarizacao": -1,
+  "politica": -1,
+  "politico": -1,
+  "pouco": -1,
+  "precisar": -1,
+  "preco": -1,
+  "processo": -1,
+  "prova": -1,
+  "repetir": -1,
+  "risco": -1,
+  "sair": -1,
+  "salario": -1,
+  "sistema": -1,
+  "temer": -1,
+  "tirar": -1,
+  "tomar": -1,
+  "absurdo": -2,
+  "arrogante": -2,
+  "bandido": -2,
+  "bolsoasno": -2,
+  "bosta": -2,
+  "bostonaro": -2,
+  "bozo": -2,
+  "bundao": -2,
+  "burro": -2,
+  "cadeia": -2,
+  "calar": -2,
+  "canalha": -2,
+  "capanga": -2,
+  "caralho": -2,
+  "centrao": -2,
+  "chorar": -2,
+  "comunista": -2,
+  "condenar": -2,
+  "contra": -2,
+  "corrupcao": -2,
+  "corrupto": -2,
+  "covarde": -2,
+  "covid": -2,
+  "crime": -2,
+  "criminoso": -2,
+  "criticar": -2,
+  "cu": -2,
+  "culpa": -2,
+  "desespero": -2,
+  "destruir": -2,
+  "ditadura": -2,
+  "enganar": -2,
+  "errar": -2,
+  "erro": -2,
+  "esquerdista": -2,
+  "fake": -2,
+  "falso": -2,
+  "familicia": -2,
+  "faminto": -2,
+  "fascismo": -2,
+  "fascista": -2,
+  "foda": -2,
+  "fome": -2,
+  "fraudar": -2,
+  "fraude": -2,
+  "gado": -2,
+  "gastar": -2,
+  "genocida": -2,
+  "golpe": -2,
+  "golpear": -2,
+  "golpista": -2,
+  "gritar": -2,
+  "guerra": -2,
+  "idiota": -2,
+  "ignorante": -2,
+  "impeachment": -2,
+  "inflacao": -2,
+  "jamais": -2,
+  "ladrao": -2,
+  "lixo": -2,
+  "luladrao": -2,
+  "lulista": -2,
+  "mal": -2,
+  "maluco": -2,
+  "matar": -2,
+  "mau": -2,
+  "medo": -2,
+  "mentir": -2,
+  "mentira": -2,
+  "mentiroso": -2,
+  "merda": -2,
+  "miliciano": -2,
+  "molusco": -2,
+  "morer": -2,
+  "morra": -2,
+  "morrer": -2,
+  "morte": -2,
+  "nao": -2,
+  "odeiar": -2,
+  "odio": -2,
+  "orcamento": -2,
+  "pandemia": -2,
+  "pau": -2,
+  "pcc": -2,
+  "perder": -2,
+  "pessimo": -2,
+  "piada": -2,
+  "pobre": -2,
+  "porra": -2,
+  "pqp": -2,
+  "prender": -2,
+  "presidiario": -2,
+  "preso": -2,
+  "prisao": -2,
+  "problema": -2,
+  "quadrilha": -2,
+  "quadrilhao": -2,
+  "reclamar": -2,
+  "rejeicao": -2,
+  "retardada": -2,
+  "retardado": -2,
+  "roubar": -2,
+  "ruim": -2,
+  "saco": -2,
+  "safado": -2,
+  "secreto": -2,
+  "sofrer": -2,
+  "terrorista": -2,
+  "triste": -2,
+  "trouxa": -2,
+  "vagabundo": -2,
+  "venezuela": -2,
+  "vergonha": -2,
+  "violencia": -2
 }
 
 stopwords = nltk.corpus.stopwords.words("portuguese")
 palavras_irrelevantes = [*punctuation] + stopwords
 token_pontuacao = tokenize.WordPunctTokenizer()
+nlp = spacy.load("pt_core_news_lg")
+token_espaco = tokenize.WhitespaceTokenizer()
 
 dataframe = pandas.read_excel('bases/turno_2.xlsx')
 
 
 def tratar_base():
+    dataframe['texto'] = dataframe['texto'].astype(str)
     dataframe['texto'] = dataframe.texto.str \
         .lower() \
         .replace(r'[^\w\s@]', '', regex=True) \
         .replace(r'[\s]+', ' ', regex=True)
     frases_processadas = list()
     for opiniao in dataframe["texto"]:
+        if opiniao is None or len(opiniao) == 0:
+          continue
         nova_frase = list()
         mencoes_tweet = re.findall("@[A-Za-z0-9_]+", opiniao)
         outras_mencoes = list(filter((lambda x: x not in arroba_candidato), mencoes_tweet))
@@ -207,11 +578,16 @@ def mostrar_grafico_palavras(quantidade):
 
 
 def aplicar_analise_sentimentos():
-    nv = NaiveBayesAnalyzer()
-    dataframe['porcentagem_positiva'] = dataframe['texto_tratado']\
-        .map(lambda texto: TextBlob(texto, analyzer=nv).sentiment.p_pos)
-    dataframe['sentimento'] = dataframe['porcentagem_positiva']\
-        .map(obter_sentimento)
+    polaridade_tweets = []
+    for tweet in dataframe['texto_tratado'].astype(str):
+      pontuacao_tweet = 0
+      palavras = tweet.split()
+      for palavra in palavras:
+        if palavra in palavras_classificadas:
+          pontuacao_tweet += palavras_classificadas[palavra] / len(tweet)
+      polaridade_tweets.append(pontuacao_tweet * len(tweet))
+    dataframe['classificacao'] = polaridade_tweets
+    dataframe.to_excel('bases/base-classificada.xlsx')
 
 
 def obter_sentimento(porcentagem_positiva):
@@ -250,7 +626,7 @@ def mostrar_tweets_unicos_ou_conjunto():
 
 
 def mostrar_mais_tweets_positivos():
-    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & sentimento > 3')
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & classificacao > 0')
     df = pandas.melt(dataframe_filtrado, value_vars=list(termos_candidatos.keys()), var_name='candidato', value_name='citacoes')
     grafico = sns.countplot(data=df.loc[df['citacoes'] == 1], x='candidato')
     grafico.set_xlabel('Candidato')
@@ -260,7 +636,7 @@ def mostrar_mais_tweets_positivos():
 
 
 def mostrar_mais_tweets_negativos():
-    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & sentimento < 3')
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & classificacao < 0')
     df = pandas.melt(dataframe_filtrado, value_vars=list(termos_candidatos.keys()), var_name='candidato', value_name='citacoes')
     grafico = sns.countplot(data=df.loc[df['citacoes'] == 1], x='candidato')
     grafico.set_xlabel('Candidato')
@@ -270,7 +646,7 @@ def mostrar_mais_tweets_negativos():
 
 
 def mostrar_tweets_positivos_por_semana() -> None:
-    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & sentimento > 3')
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & sentimento > 0')
     df = pandas.DataFrame(columns=['semana', 'candidato', 'quantidade'])
     semanas_analise = dataframe_filtrado['semana_analise'].unique()
     for semana in semanas_analise:
@@ -295,10 +671,36 @@ def mostrar_palavras_mais_usadas():
     token_frase = token_espaco.tokenize(todas_palavras)
     frequencia = nltk.FreqDist(token_frase)
     df_frequencia = pandas.DataFrame({"palavra": list(frequencia.keys()), "frequencia": list(frequencia.values())})
-    df_frequencia.to_excel('bases/palavras-mais-usadas.xlsx')
+    df_frequencia.to_excel('bases/palavras_mais_usadas_periodo_completo.xlsx')
+    
+
+def mostrar_volumetria_classificacao(candidato):
+  quantidade_neutro, quantidade_positivo, quantidade_negativo = 0, 0, 0
+  df_candidato = dataframe.query(f'{candidato} == 1 & mais_de_um_candidato == 0')
+  for classificacao in df_candidato['classificacao'].astype(float):
+    if classificacao > 0:
+      quantidade_positivo += 1
+      continue
+    if classificacao < 0:
+      quantidade_negativo += 1
+      continue
+    quantidade_neutro += 1
+  legenda = 'Positivo', 'Negativo', 'Neutro'
+  cores = ["#23C552", "#F84F31", "#808080"]
+  quantidade_geral = [quantidade_positivo, quantidade_negativo, quantidade_neutro]
+  plt.pie(quantidade_geral,
+        labels=legenda,
+        autopct='%1.1f%%',
+        colors = cores,
+        shadow = True)
+  print(f'Positivos: {quantidade_positivo}')
+  print(f'Negativos: {quantidade_negativo}')
+  print(f'Neutros: {quantidade_neutro}')
+  plt.legend(title = "Sentimento")
+  plt.show()
 
 
-tratar_base()
+# tratar_base()
 mostrar_palavras_mais_usadas()
 aplicar_analise_sentimentos()
 mostrar_contagem_citacoes()
@@ -310,3 +712,4 @@ mostrar_tweets_unicos_ou_conjunto()
 mostrar_mais_tweets_positivos()
 mostrar_mais_tweets_negativos()
 mostrar_tweets_positivos_por_semana()
+mostrar_volumetria_classificacao('lula')
