ebdfe2ae804342e834059e0072cfe660786767b0
andrey <andreywill9@gmail.com>
2022-11-08 19:05:57 -0300

    Refatoração completa do código e dividindo em módulos

---
+++ graficos.py
@@ -0,0 +1,213 @@
+import matplotlib
+import pandas
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import nltk
+from nltk import tokenize
+import seaborn as sns
+from wordcloud import WordCloud
+
+nltk.download('movie_reviews')
+nltk.download('punkt')
+
+token_espaco = tokenize.WhitespaceTokenizer()
+
+
+def mostrar_contagem_citacoes(dataframe, termos_candidatos: dict) -> None:
+    df = pandas.melt(dataframe, value_vars=list(termos_candidatos.keys()), var_name='candidato', value_name='citacoes')
+    plt.subplots(figsize=(20, 10))
+    grafico = sns.countplot(data=df.loc[df['citacoes'] == 1], x='candidato', color="#ffa500")
+    grafico.set_xlabel('Candidato')
+    grafico.set_ylabel('Número de citações')
+    grafico.set_title("Total de menções por candidato")
+    sns.set(font_scale=1)
+    for p in grafico.patches:
+        grafico.annotate(p.get_height(), (p.get_x() + 0.25, p.get_height() + 0.01))
+    plt.show()
+
+
+def mostrar_citacoes_por_semana(dataframe, termos_candidatos: dict) -> None:
+    df = pandas.DataFrame(columns=['semana', 'candidato', 'quantidade'])
+    semanas_analise = dataframe['semana_analise'].unique()
+    for semana in semanas_analise:
+        for candidato in termos_candidatos.keys():
+            df2 = dataframe.query(f'semana_analise == {semana} & {candidato} == 1')
+            quantidade = len(df2.index)
+            df = pandas.concat([df, pandas.DataFrame.from_records([{
+                'semana': semana,
+                'candidato': candidato,
+                'quantidade': quantidade
+            }])])
+
+    df3 = df.pivot(index='semana', columns='candidato', values='quantidade')
+    df3.plot(figsize=(20, 10),
+             lw=3,
+             kind='line',
+             style='s:',
+             title="Menções por semana")
+    plt.xlabel('Semana')
+    plt.ylabel('Número de citações')
+    plt.legend(title='Candidato', bbox_to_anchor=(1, 1))
+    plt.show()
+
+
+def mostrar_citacoes_por_dia(dataframe, termos_candidatos: dict) -> None:
+    print()
+    # TODO
+
+
+def mostrar_rival_natural(dataframe, termos_candidatos: dict) -> None:
+    df = pandas.DataFrame(columns=['candidatos', 'citacoes'])
+    df.set_index('candidatos')
+    for candidato in termos_candidatos.keys():
+        for candidato2 in termos_candidatos.keys():
+            combinacao = f'{candidato} e {candidato2}'
+            combinacao_invertida = f'{candidato2} e {candidato}'
+            if candidato == candidato2 or combinacao in df.candidatos.values or combinacao_invertida in df.candidatos.values:
+                continue
+            df2 = dataframe.query(f'{candidato} == 1 & {candidato2} == 1')
+            contagem = len(df2.index)
+            df = pandas.concat([df, pandas.DataFrame.from_records([{
+                'candidatos': f'{candidato} e {candidato2}',
+                'citacoes': contagem
+            }])])
+    df = df.sort_values(by='citacoes', ascending=False, ignore_index=True)
+    df = df[:10]
+    df.set_index('candidatos', inplace=True)
+    df.plot(figsize=(20, 10),
+            kind='barh',
+            title="Maiores menções em conjunto (""Rivais Naturais"")").legend(loc='upper center')
+    plt.xlabel('Número de citações')
+    plt.ylabel('Rivais naturais')
+    plt.xticks(rotation=0, horizontalalignment="center")
+    plt.show()
+
+
+def mostrar_grafico_palavras(dataframe, quantidade: int):
+    todas_palavras = ' '.join([texto for texto in dataframe['texto']])
+    token_frase = token_espaco.tokenize(todas_palavras)
+    frequencia = nltk.FreqDist(token_frase)
+    df_frequencia = pandas.DataFrame({"palavra": list(frequencia.keys()), "frequencia": list(frequencia.values())})
+    df_frequencia.nlargest(columns="frequencia", n=quantidade)
+    df_frequencia = df_frequencia.nlargest(columns="frequencia", n=10)
+    plt.figure(figsize=(15, 10))
+    grafico = sns.barplot(data=df_frequencia, x="palavra", y="frequencia")
+    grafico.set(ylabel="Contagem")
+    grafico.set_title("Total de citações por candidato")
+    plt.show()
+
+
+def mostrar_nuvem_palavras(dataframe):
+    todas_palavras = ' '.join([texto for texto in dataframe.texto_tratado.astype(str)])
+    nuvem_palavras = WordCloud(width=2000,
+                               height=1000,
+                               colormap="Dark2",
+                               background_color="#fefff2",
+                               collocations=False).generate(todas_palavras)
+
+    plt.figure(figsize=(20, 10))
+    plt.imshow(nuvem_palavras, interpolation='bilinear')
+    plt.axis("off")
+    plt.title("Palavras mais usadas durante as eleições no Twitter")
+    plt.show()
+
+
+def mostrar_tweets_unicos_ou_conjunto(dataframe):
+    quantidade_tweets_unicos = len(dataframe.query('mais_de_um_candidato == 0'))
+    quantidade_tweets_conjunto = len(dataframe.query('mais_de_um_candidato == 1'))
+
+    qtde = [quantidade_tweets_unicos, quantidade_tweets_conjunto]
+    legenda = 'Único', 'Conjunto'
+    cores = ["#e7c137", "#a6d1ff"]
+    explosao = [0.1, 0]
+    fig, chart = plt.subplots(figsize=(10, 8))
+    chart.pie(qtde,
+              labels=legenda,
+              autopct='%1.1f%%',
+              colors=cores,
+              explode=explosao,
+              startangle=90,
+              shadow=True)
+    chart.set_title("Menções dos candidatos")
+    plt.legend(title="")
+    plt.show()
+
+
+def mostrar_mais_tweets_positivos(dataframe, termos_candidatos: dict):
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & classificacao > 0')
+    df = pandas.melt(dataframe_filtrado, value_vars=list(termos_candidatos.keys()), var_name='candidato',
+                     value_name='citacoes')
+    plt.subplots(figsize=(20, 10))
+    grafico = sns.countplot(data=df.loc[df['citacoes'] == 1], x='candidato', color="#23C552")
+    grafico.set_xlabel('Candidato')
+    grafico.set_ylabel('Número de citações positivas')
+    grafico.set_title("Total de citações positivas")
+    sns.set(font_scale=1)
+    for p in grafico.patches:
+        grafico.annotate(p.get_height(), (p.get_x() + 0.25, p.get_height() + 0.01))
+    plt.show()
+
+
+def mostrar_mais_tweets_negativos(dataframe, termos_candidatos: dict):
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & classificacao < 0')
+    df = pandas.melt(dataframe_filtrado, value_vars=list(termos_candidatos.keys()), var_name='candidato',
+                     value_name='citacoes')
+    plt.subplots(figsize=(20, 10))
+    grafico = sns.countplot(data=df.loc[df['citacoes'] == 1], x='candidato', color="#F84F31")
+    grafico.set_xlabel('Candidato')
+    grafico.set_ylabel('Número de citações negativas')
+    grafico.set_title("Total de citações negativas")
+    sns.set(font_scale=1)
+    for p in grafico.patches:
+        grafico.annotate(p.get_height(), (p.get_x() + 0.25, p.get_height() + 0.01))
+    plt.show()
+
+
+def mostrar_tweets_positivos_por_semana(dataframe, termos_candidatos: dict) -> None:
+    dataframe_filtrado = dataframe.query('mais_de_um_candidato == 0 & sentimento > 0')
+    df = pandas.DataFrame(columns=['semana', 'candidato', 'quantidade'])
+    semanas_analise = dataframe_filtrado['semana_analise'].unique()
+    for semana in semanas_analise:
+        for candidato in termos_candidatos.keys():
+            df2 = dataframe_filtrado.query(f'semana_analise == {semana} & {candidato} == 1')
+            quantidade = len(df2.index)
+            df = pandas.concat([df, pandas.DataFrame.from_records([{
+                'semana': semana,
+                'candidato': candidato,
+                'quantidade': quantidade
+            }])])
+    df3 = df.pivot(index='semana', columns='candidato', values='quantidade')
+    df3.plot()
+    plt.xlabel('Semana')
+    plt.ylabel('Número de citações')
+    plt.legend(title='Candidato', bbox_to_anchor=(1, 1))
+    plt.show()
+
+
+def mostrar_volumetria_classificacao(dataframe):
+    quantidade_neutro, quantidade_positivo, quantidade_negativo = 0, 0, 0
+    df_candidato = dataframe.query(f'lula == 1 & mais_de_um_candidato == 0')
+    for classificacao in df_candidato['classificacao'].astype(float):
+        if classificacao > 0:
+            quantidade_positivo += 1
+            continue
+        if classificacao < 0:
+            quantidade_negativo += 1
+            continue
+        quantidade_neutro += 1
+
+    legenda = 'Positivo', 'Negativo', 'Neutro'
+    cores = ["#23C552", "#F84F31", "#808080"]
+    quantidade_geral = [quantidade_positivo, quantidade_negativo, quantidade_neutro]
+
+    fig, chart = plt.subplots(figsize=(10, 8))
+
+    chart.pie(quantidade_geral,
+              labels=legenda,
+              autopct='%1.1f%%',
+              colors=cores,
+              shadow=True)
+    chart.set_title("Sentimentos")
+    plt.legend(title="Sentimento")
+    plt.show()
---
+++ tratamento_bases.py
@@ -0,0 +1,115 @@
+from constantes import arroba_candidato
+from constantes import stopwords
+from constantes import abreviacoes
+from constantes import palavras_irrelevantes
+from constantes import palavras_classificadas
+from constantes import termos_todos_candidatos
+from constantes import token_espaco
+import pandas
+import nltk
+import re
+import unidecode
+import spacy
+import itertools
+
+nltk.download('movie_reviews')
+nltk.download('punkt')
+
+nlp = spacy.load("pt_core_news_lg")
+
+
+def tratar_base(dataframe: pandas.DataFrame):
+    dataframe['texto'] = dataframe['texto'].astype(str)
+    dataframe['texto'] = dataframe.texto.str \
+        .lower() \
+        .replace(r'[^\w\s@]', '', regex=True) \
+        .replace(r'[\s]+', ' ', regex=True)
+    frases_processadas = list()
+    for opiniao in dataframe["texto"]:
+        if opiniao is None or len(opiniao) == 0:
+            continue
+        nova_frase = list()
+        mencoes_tweet = re.findall("@[A-Za-z0-9_]+", opiniao)
+        outras_mencoes = list(filter((lambda x: x not in arroba_candidato), mencoes_tweet))
+        palavras_texto2 = opiniao.split(' ')
+        for palavra in palavras_texto2:
+            if re.match(r'^(k|ha|rs|lol|he)+', palavra):
+                continue
+            if palavra in outras_mencoes or palavra in stopwords or set(
+                    abreviacoes[palavra].split(' ') if palavra in abreviacoes.keys() else [palavra]).issubset(stopwords):
+                continue
+            if palavra in arroba_candidato:
+                nova_frase.append(arroba_candidato[palavra])
+                continue
+            palavra = unidecode.unidecode(palavra)
+            if palavra in palavras_irrelevantes:
+                continue
+            nova_frase.extend(abreviacoes[palavra].split(' ') if palavra in abreviacoes.keys() else [palavra])
+        doc = nlp(' '.join(nova_frase))
+        nova_frase = list()
+        for palavra in doc:
+            nova_frase.append(palavra.lemma_.lower())
+        frases_processadas.append(' '.join(nova_frase))
+    dataframe['texto_tratado'] = frases_processadas
+    dataframe['data_tweet'] = pandas.to_datetime(dataframe['data_tweet']) \
+        .dt.tz_localize(None)
+    dataframe['mes'] = dataframe['data_tweet'].dt.month
+    dataframe['semana_ano'] = dataframe['data_tweet'].map(lambda data: int(data.strftime("%V")))
+    dataframe['semana_analise'] = dataframe['semana_ano'] - 22
+    for chave, valor in termos_todos_candidatos.items():
+        termos_regex = '|'.join(valor)
+        dataframe[chave] = dataframe['texto_tratado'].str.contains(pat=fr'(?:{termos_regex.lower()})', case=False).map(
+            {True: 1,
+             False: 0})
+    dataframe['mais_de_um_candidato'] = dataframe['lula'] + dataframe['bolsonaro'] + dataframe['ciro'] \
+                                        + dataframe['simone_tebet'] + dataframe['eymael'] \
+                                        + dataframe['sofia_manzano'] + dataframe['soraya_thronicke'] \
+                                        + dataframe['felipe_d_avila'] + dataframe['vera_lucia'] \
+                                        + dataframe['leo_pericles']
+    dataframe['mais_de_um_candidato'] = dataframe['mais_de_um_candidato'] > 1
+
+
+def aplicar_analise_sentimentos(dataframe: pandas.DataFrame):
+    polaridade_tweets = []
+    nomes_candidatos = itertools.chain.from_iterable(termos_todos_candidatos.values())
+    for tweet in dataframe['texto_tratado'].astype(str):
+        pontuacao_tweet = 0
+        palavras = tweet.split()
+        quantidade_mencoes = len(list(filter(lambda x: x in nomes_candidatos, palavras)))
+        for palavra in palavras:
+            if palavra in palavras_classificadas:
+                pontuacao_tweet += palavras_classificadas[palavra] * (1 / (len(palavras) - quantidade_mencoes))
+        polaridade_tweets.append(obter_pontuacao(pontuacao_tweet))
+    dataframe['classificacao'] = polaridade_tweets
+
+
+def obter_pontuacao(pontuacao_calculada):
+    if pontuacao_calculada == 0:
+        return 0
+    if pontuacao_calculada < -1:
+        return -2
+    if pontuacao_calculada < 0:
+        return -1
+    if pontuacao_calculada < 1:
+        return 1
+    return 2
+
+
+def salvar_palavras_mais_usadas(dataframe: pandas.DataFrame, caminho_saida: str):
+    todas_palavras = ' '.join([texto for texto in dataframe['texto_tratado'].astype(str)])
+    token_frase = token_espaco.tokenize(todas_palavras)
+    frequencia = nltk.FreqDist(token_frase)
+    df_frequencia = pandas.DataFrame({"palavra": list(frequencia.keys()), "frequencia": list(frequencia.values())})
+    df_frequencia.to_excel(caminho_saida)
+
+
+dataframes = {
+    'turno_1': pandas.read_excel('bases/turno_1.xlsx'),
+    'turno_2': pandas.read_excel('bases/turno_2.xlsx'),
+    'periodo_eleitoral_completo': pandas.read_excel('bases/periodo_eleitoral_completo.xlsx'),
+    'pre_eleitoral': pandas.read_excel('bases/pre_eleitoral.xlsx')
+}
+for nome_dataframe in dataframes:
+    tratar_base(dataframes[nome_dataframe])
+    aplicar_analise_sentimentos(dataframes[nome_dataframe])
+    dataframes[nome_dataframe].to_excel(f'bases/{nome_dataframe}_tratado.xlsx')
